dc9453beabb4ebbc2f049f98fe800c35cc66e734
scottklein7 <scott.klein02@protonmail.com>
2021-12-03 15:09:11 -0500

    Adds update route for biceptricep

--- controllers/muscle.js
+++ controllers/muscle.js
@@ -252,7 +252,27 @@ muscleRouter.put('/chest/:id', (req, res) => {
 
 // update back 
 muscleRouter.put('/back/:id', (req, res) => {
-    req.body.completed = !! req.body.completed
+    req.body.completed = !!req.body.completed
+    Exercise.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, (err, exercise) => {
+        res.redirect(`/${exercise.muscleGroup}/${req.params.id}`)
+    })
+})
+
+// update bicep
+muscleRouter.put('/bicep/:id', (req, res) => {
+    req.body.completed = !!req.body.completed
+    Exercise.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, (err, exercise) => {
+        res.redirect(`/${exercise.muscleGroup}/${req.params.id}`)
+    })
+})
+
+// update tricep
+muscleRouter.put('/tricep/:id', (req, res) => {
+    req.body.completed = !!req.body.completed
     Exercise.findByIdAndUpdate(req.params.id, req.body, {
         new: true
     }, (err, exercise) => {

a15ad95c22af096fc5b203c26556861fb48a0e2e
scottklein7 <scott.klein02@protonmail.com>
2021-12-13 17:51:43 -0500

    Updates for Heroku

--- controllers/muscle.js
+++ controllers/muscle.js
@@ -5,70 +5,7 @@ const Exercise = require('../models/exercise')
 const exercise = require('../models/exercise')
 const muscleRouter = express.Router()
 
-// routes
-
-// seed
-muscleRouter.get('/seed', async (req, res) => {
-    const Data = [
-
-        {
-            user_id: "61b2b31c72d296f0a573e682",
-            muscleGroup: "Chest",
-            exercise: "FIRST ONE",
-            sets: 3,
-            reps: 10,
-            rpe: 8,
-            rest: 2,
-            weight: 200,
-            notes: "went hard on this one",
-            completed: true
-        },
-
-        {
-            user_id: "61b2b31c72d296f0a573e682",
-            muscleGroup: "chest",
-            exercise: "SECOND ONE",
-            sets: 4,
-            reps: 9,
-            rpe: 8,
-            rest: 2,
-            weight: 0,
-            notes: "this was hard",
-            completed: true
-        },
-
-        {
-            user_id: "61b2b378218835f2f6529fec",
-            muscleGroup: "Tricep",
-            exercise: "Dips",
-            sets: 2,
-            reps: 16,
-            rpe: 7,
-            rest: 2,
-            weight: 0,
-            notes: "Almost failed",
-            completed: true
-        },
-        {
-            user_id: "61b2b378218835f2f6529fec",
-            muscleGroup: "Leg",
-            exercise: "Sqaut",
-            sets: 4,
-            reps: 9,
-            rpe: 7,
-            rest: 3,
-            weight: 250,
-            notes: "almost hurt my back",
-            completed: true
-        },
-    ]
-    await Exercise.deleteMany({});
-    await Exercise.create(Data);
-    res.redirect('/')
-})
-
-
-// INDUCES
+// --------ROUTES-------- //
 
 // --------INDEX-------- //
 muscleRouter.get('/:user_id/:exercise', (req, res) => {
@@ -97,8 +34,6 @@ muscleRouter.get('/:user_id/:exercise', (req, res) => {
 
 // --------NEW-------- //
 muscleRouter.get('/:user_id/:exercise/new', (req, res) => {
-    console.log(req.session.user, 'is the user!!!!!!!!!')
-    console.log(req.params.exercise)
     res.render('exercises/newExercise.ejs', {
         Exercise: req.params.exercise,
         tabTitle: 'New Exercise',
@@ -106,14 +41,14 @@ muscleRouter.get('/:user_id/:exercise/new', (req, res) => {
     })
 })
 
-// // --------DELETE--------//
+//--------DELETE--------//
 muscleRouter.delete('/:user_id/:exercise/:id', (req, res) => {
     Exercise.findByIdAndDelete(req.params.id, (err, exercise) => {
         res.redirect(`/${req.session.user}/${req.params.exercise}`)
     })
 })
 
-// // --------UPDATE--------//
+// --------UPDATE--------//
 muscleRouter.put('/:user_id/:exercise/:id', (req, res) => {
     req.body.completed = !!req.body.completed
     Exercise.findByIdAndUpdate(req.params.id, req.body, {
@@ -123,7 +58,7 @@ muscleRouter.put('/:user_id/:exercise/:id', (req, res) => {
     })
 })
 
-// // --------CREATE--------//
+// --------CREATE--------//
 muscleRouter.post('/', (req, res) => {
     req.body.completed = !!req.body.completed
     Exercise.create(req.body, (err, exercise) => {
@@ -153,7 +88,7 @@ muscleRouter.get('/:user_id/:exercise/:id/editexercise', (req, res) => {
     })
 })
 
-// // --------SHOW--------//
+// --------SHOW--------//
 muscleRouter.get('/:user_id/:exercise/:id', (req, res) => {
     Exercise.findById(req.params.id, (err, exercise) => {
         res.render('exercises/showExercise.ejs', {
